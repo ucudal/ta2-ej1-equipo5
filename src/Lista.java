1713f0fb4cc5ee2c6008c6f293d16ffb1c343cef
Juan Nocetti <juan.anocetti@gmail.com>
2022-08-31 19:23:02 -0300

    parte1

--- src/Lista.java
+++ src/Lista.java
@@ -8,4 +8,64 @@ public class Lista<T> implements ILista<T> {
 
 
     // implementar los metodos indicados en la interfaz
+
+    @Override
+    public void insertar(Nodo<T> nodo) {
+        if (esVacia()){
+            primero=nodo;
+            return;
+        }
+        Nodo<T> aux = primero;
+        while (aux.getSiguiente() != null){
+            aux=aux.getSiguiente();
+        }
+        aux.setSiguiente(nodo);
+    }
+    @Override
+    public Nodo<T> buscar(Comparable clave) {
+        if (esVacia()){
+            return null;
+        }
+        Nodo<T> aux= primero;
+        while (aux!=null){
+            if (aux.getEtiqueta().equals(clave)){
+                return aux;
+            }
+           aux=aux.getSiguiente();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean eliminar(Comparable clave) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String imprimir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String imprimir(String separador) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int cantElementos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean esVacia() {
+        if (primero==null){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void setPrimero(Nodo<T> unNodo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
---
+++ src/Producto.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 59891
+ */
+public class Producto implements IProducto {
+    public String nombre;
+    public int codigo;
+    public float precio;
+    public int stock;
+    public Producto(String nombre,int codigo,float precio, int stock){
+        this.nombre=nombre;
+        this.codigo=codigo;
+        this.precio=precio;
+        this.stock=stock;
+    }
+
+    @Override
+    public Comparable getCodProducto() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Integer getPrecio() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setPrecio(Integer precio) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Integer getStock() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void agregarCantidadStock(Integer stock) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void restarCantidadStock(Integer stock) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
